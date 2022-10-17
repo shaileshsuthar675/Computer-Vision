5b04d2a8740ff072016fb3ac0752734bad355ef3
Shailesh Suthar <91286534+shaileshsuthar675@users.noreply.github.com>
2022-10-17 12:17:29 +0530

    Add files via upload

---
+++ Pong Game/Hand_tracking_module.py
@@ -0,0 +1,173 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+import numpy as np
+
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+
+    def findHands(self, img, draw=True, img_flip=False):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        all_hand = []
+        if self.results.multi_hand_landmarks:
+            for hand_type, hands_lm in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
+
+                my_hands = {}
+                lm_list = []
+                xList = []
+                yList = []
+                bbox = []
+
+                for id, lm in enumerate(hands_lm.landmark):
+                    # print(id, lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    xList.append(cx)
+                    yList.append(cy)
+                    # print(id, cx, cy)
+                    lm_list.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                ## bbox
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), \
+                         bbox[1] + (bbox[3] // 2)
+
+                my_hands['lm_list'] = lm_list
+                my_hands['b_box'] = bbox
+                my_hands['center'] = [cx, cy]
+
+                if img_flip == False:
+                    if hand_type.classification[0].label == "Right":
+                        my_hands["type"] = "Left"
+                    else:
+                        my_hands["type"] = "Right"
+                else:
+                    my_hands["type"] = hand_type.classification[0].label
+                all_hand.append(my_hands)
+                ## draw
+                if draw:
+                    self.mpDraw.draw_landmarks(img, hands_lm,
+                                               self.mpHands.HAND_CONNECTIONS)
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (0, 255, 0), 2)
+                    cv2.putText(img, my_hands["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                                2, (0, 255, 0), 2)
+        if draw:
+            return all_hand, img
+        else:
+            return all_hand
+
+    def fingersUp(self):
+        fingers = []
+        # Thumb
+        for i in self.results.multi_handedness:
+            for id, score in enumerate(i.classification):
+                # print(id, score)
+                index_ = score.index
+        if index_ == 0:
+            if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        if index_ == 1:
+            if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        # Fingers
+        for id in range(1, 5):
+
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # totalFingers = fingers.count(1)
+
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        return length, img, [x1, y1, x2, y2, cx, cy]
+
+    def overlayPNG(self, imgBack, imgFront, pos=[0, 0]):
+        hf, wf, cf = imgFront.shape
+        hb, wb, cb = imgBack.shape
+        *_, mask = cv2.split(imgFront)
+        maskBGRA = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+        maskBGR = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+        imgRGBA = cv2.bitwise_and(imgFront, maskBGRA)
+        imgRGB = cv2.cvtColor(imgRGBA, cv2.COLOR_BGRA2BGR)
+
+        imgMaskFull = np.zeros((hb, wb, cb), np.uint8)
+        imgMaskFull[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = imgRGB
+        imgMaskFull2 = np.ones((hb, wb, cb), np.uint8) * 255
+        maskBGRInv = cv2.bitwise_not(maskBGR)
+        imgMaskFull2[pos[1]:hf + pos[1], pos[0]:wf + pos[0], :] = maskBGRInv
+
+        imgBack = cv2.bitwise_and(imgBack, imgMaskFull2)
+        imgBack = cv2.bitwise_or(imgBack, imgMaskFull)
+
+        return imgBack
+
+    def overlay_3_channel(self, imgBack, imgFront, pos=[0, 0]):
+        h, w, c = imgFront.shape
+        # print(h, w)
+        imgBack[pos[1]:pos[1] + h, pos[0]:pos[0] + w] = imgFront
+        return imgBack
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = handDetector()
+    while True:
+        success, img = cap.read()
+        hands, img = detector.findHands(img)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
